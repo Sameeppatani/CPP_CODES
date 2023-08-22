f82aa03f0a0645a72e3f30eb825c59382cf1ef6c
sameep <patanisameep6@gmail.com>
2023-08-22 20:20:51 +0530

    insertion and deletion of ith element

--- LINKEDLIST/LinkedList_insertion_and_deletionof_ith_element.cpp
+++ LINKEDLIST/LinkedList_insertion_and_deletionof_ith_element.cpp
@@ -45,9 +45,50 @@ node*takeinput(){
     return head;
 
 }
+node * insertnode(node*head,int n,int data){
+    node*newnode=new node(data);
+    int i=0;
+    node*temp=head;
+    if(n==0){
+        newnode->next=head;
+        head=newnode;
+        return head;
+    }
+    while(temp!=NULL && i<n-1){
+        temp=temp->next;
+        i++;
+    }
+    if(temp!=NULL){
+    node*a=temp->next;
+    temp->next=newnode;
+    newnode->next=a;
+}
+return head;
+}
+void deletenode(node*head,int n){
+    int i=0;
+    node*temp=head;
+    while(i<n-1 && temp!=NULL){
+            temp=temp->next;
+        i++;
+    }
+    if(temp!=NULL && temp->next!=NULL){
+        node*a=temp->next;
+        temp->next=a->next;
+        delete a;
+
+    }
+
+
+}
 
 int main(){
     node*head=takeinput();
+    print(head);
+
+    head=insertnode(head,5,1000);
+    print(head);
+    deletenode(head,1);
     print(head);
 
 
